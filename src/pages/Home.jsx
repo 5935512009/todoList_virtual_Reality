40811a261cbe079ce3faaa046e461960418f6b10
5935512009 <Pasut2541@hotmail.com>
2025-02-23 10:31:14 +0700

    update: {basic add todoList and show list}

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,35 @@
 import React,{useState} from "react";
-
+import './home.css'
 function Home() {
-    const [todos,setTodos] = useState([]);
+    const [todos,setTodos] = useState([]); // set todos list array
     console.log(todos);
+
+    // function input todolist
+    function addTodo(){
+        const input = document.getElementById("todoList_input").value; // get value
+        if(input ==="") return; // if no value return
+        console.log("input before change"+input);
+        const newTodo = {
+            id: Date.now(),
+            text: input
+        }
+        // list todo (ต่อ query)
+        setTodos([...todos,newTodo])
+
+        // clear value
+        document.getElementById("todoList_input").value="";
+    }
+
+
   return (
     <div className="home">
       <div className="home_main">Virtual Reality Todolist</div>
       <div className="home_details">
         <div className="home_details_input">
           <input type="text" id="todoList_input"/>
-          <button className="submit_button" onClick={(e)=>setTodos(document.getElementById("todoList_input").value)}>Add Todo</button>
+          <button className="submit_button" onClick={addTodo}>Add Todo</button>
         </div>
-        <ul>
+        <ul className="todo">
             {todos.map((todo)=>(
                 <li key={todo.id}>{todo.text}</li>
             ))}
